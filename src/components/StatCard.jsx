9ddd6b56b497111189efb5205c743d98928ab212
antonioval-sys <antonio.drinkcafe@gmail.com>
2026-01-26 12:31:20 -0600

    mod-vent

---
+++ src/components/StatCard.jsx
@@ -0,0 +1,9 @@
+export default function StatCard({ titulo, valor }) {
+  return (
+    <div>
+      <h3>{titulo}</h3>
+      <p>{valor}</p>
+    </div>
+  )
+}
+
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,15 @@
+import StatCard from '../components/StatCard'
 export default function Dashboard() {
-  return (
+ return (
     <main>
-      <p>Contenido principal</p>
+      <h2>Resumen de Ventas</h2>
+
+      <div>
+        <StatCard titulo="Ventas del día" valor="$12,500" />
+        <StatCard titulo="Ventas del mes" valor="$245,000" />
+        <StatCard titulo="Órdenes" valor="120" />
+        <StatCard titulo="Clientes" valor="48" />
+      </div>
     </main>
   )
 }
